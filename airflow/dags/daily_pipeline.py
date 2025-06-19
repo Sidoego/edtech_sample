8d114ab19eabf496b4a539391ea99bb4adb5109e
Sidoego <sidoego@gmail.com>
2025-06-19 12:32:21 +0700

    Update daily_pipeline.py

--- airflow/dags/daily_pipeline.py
+++ airflow/dags/daily_pipeline.py
@@ -18,7 +18,15 @@ with DAG(
 
     extract = BashOperator(
         task_id='extract_events',
-        bash_command='python /opt/scripts/extract_from_s3.py {{ ds }}'
+        bash_command='python /opt/scripts/extract_from_s3.py {{ ds }} /tmp/raw_events_{{ ds }}.json'
+    )
+
+    load_raw = BashOperator(
+        task_id='load_raw_events',
+        bash_command=(
+            "clickhouse-client --query=\"INSERT INTO raw.events_raw FORMAT JSONEachRow\" "
+            "< /tmp/raw_events_{{ ds }}.json"
+        )
     )
 
     run_dbt = BashOperator(
@@ -26,4 +34,4 @@ with DAG(
         bash_command='cd /opt/dbt && dbt run --models +* --vars "{\"run_date\":\"{{ ds }}\"}"'
     )
 
-    extract >> run_dbt
+    extract >> load_raw >> run_dbt
